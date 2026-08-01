83606243099247aabd48b3de33dd574430080761
cheah <saturd67@gmail.com>
2026-08-01 18:08:20 +0800

    initial commit

--- run_admin_portal.bat
+++ run_admin_portal.bat
@@ -1,11 +1,6 @@
 @echo off
-REM Go to the directory where this batch file is located
-cd /d %~dp0
+cd /d "%~dp0"
 
-REM Activate the virtual environment
-call .venv\Scripts\activate
-
-REM Run the Python script
-python admin_portal.py
+.venv\Scripts\python.exe admin_portal.py
 
 pause
--- run_user_portal.bat
+++ run_user_portal.bat
@@ -1,11 +1,6 @@
 @echo off
-REM Go to the directory where this batch file is located
-cd /d %~dp0
+cd /d "%~dp0"
 
-REM Activate the virtual environment
-call .venv\Scripts\activate
-
-REM Run the Python script
-python user_portal.py
+.venv\Scripts\python.exe user_portal.py
 
 pause
